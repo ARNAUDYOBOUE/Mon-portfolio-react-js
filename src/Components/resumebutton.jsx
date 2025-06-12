c31c6a1388af01c7742be7a15af750768f82db51
ARNAUD YOBOUE <arnaudyoboue1@yahoo.com>
2025-06-12 16:35:07 +0000

    ajout de CV

--- src/Components/resumebutton.jsx
+++ src/Components/resumebutton.jsx
@@ -4,8 +4,23 @@ import '../index'
 function ResumeButton() {
     return (
         <div className="buttonSubmit">
-            <button type="button" className="btn btn-outline-dark mx-2 resumeButton">Resume</button>
+            <a 
+                href="https://drive.google.com/file/d/1GhlntiKlcVgZ8bxfKPaP3Z4A65StVp8s/view?usp=sharing" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="text-decoration-none"
+                aria-label="Télécharger mon CV"
+            >
+                <button 
+                    type="button" 
+                    className="btn btn-outline-dark mx-2 resumeButton"
+                    style={{ cursor: 'pointer' }}
+                >
+                    Télécharger mon CV
+                </button>
+            </a>
         </div>
     )
 }
-export default ResumeButton
+
+export default ResumeButton
